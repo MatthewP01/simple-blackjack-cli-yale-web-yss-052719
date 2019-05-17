8ccc181aa822b5bcc3a6c99e0d0ec9acaeddb348
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 18:14:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,7 @@ def hit?(current_total)
   get_user_input
   if get_user_input == 'h'
     new_card = deal_card
-    card_total += new_card
+    current_total += new_card
 end
 
 def invalid_command
